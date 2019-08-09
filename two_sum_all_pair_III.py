cdcfcc96bb4f69aaa22eb9ee1d96489a31f66b0d
dandan224 <43757729+dandan224@users.noreply.github.com>
2019-08-08 22:05:57 -0700

    Update two_sum_all_pair_III.py
    Find all pairs of elements in a given array that sum to the pair the given target number. Return all the distinct pairs of values.
    A = {2, 1, 3, 2, 4, 3, 4, 2}, target = 6, return [[2, 4], [3, 3]]

--- two_sum_all_pair_III.py
+++ two_sum_all_pair_III.py
@@ -7,20 +7,21 @@ class Solution(object):
     # write your solution here
     array.sort()
     res = []
+    if not array: return res
+
     i, j = 0, len(array) - 1
     while i < j:
       if array[i] + array[j] == target:
         res.append([array[i], array[j]])
-        if array[i] != array[i + 1]:
+        i += 1
+        j -= 1        
+        while i < j and array[i] == array[i-1]:
           i += 1
-        else:
-          i += 2
-        if array[j] != array[j - 1]:
+        while i < j and array[j] == array[j+1]:
           j -= 1
-        else:
-          j -= 2
       elif array[i] + array[j] > target:
         j -= 1
       else:
         i += 1
+      
     return res
